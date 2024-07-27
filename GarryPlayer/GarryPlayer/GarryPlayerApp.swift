e153375d4566a9afc6b5aa2aafdd399fb216ff3f
KurKing <lyosha.kurkin@gmail.com>
2024-07-27 11:01:38 +0300

    - initial commit

---
+++ GarryPlayer/GarryPlayer/GarryPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarryPlayerApp.swift
+//  GarryPlayer
+//
+//  Created by Oleksii on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GarryPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
